74b2709983e99128e662a04379f544a21b7da1d3
sehrishbashir <49878787+sehrishbashir@users.noreply.github.com>
2019-07-09 15:54:19 +0500

    Add files via upload

---
+++ script .js
@@ -0,0 +1,12 @@
+const days = ['Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat']
+
+
+console.log(days);
+
+
+days.forEach(function (day, index) {
+    console.log(`starts with ${index+1} == ${day}`);
+             
+})
+
+
